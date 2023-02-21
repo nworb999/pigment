4ca59ba37acae47c03a60575556ea8df2fcbf73e
nworb9 <emma.nworb@gmail.com>
2023-02-20 19:55:12 -0500

    Add html title:

--- src/utils.js
+++ src/utils.js
@@ -38,6 +38,7 @@ export const createPigment = (input) => {
   return `
   <html lang="en">
   <head>
+    <title>barnyard</title>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="icon" href="/public/favicon.png" type="image/png" />
